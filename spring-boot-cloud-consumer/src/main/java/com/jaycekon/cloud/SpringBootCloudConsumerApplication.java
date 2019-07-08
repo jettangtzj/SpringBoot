9cb460a5fe9c0a0458e57731657ee9095b1a76d3
jet tang <jettang@jetdeMacBook-Pro.local>
2019-07-08 18:39:01 +0800

    add comments

--- spring-boot-cloud-consumer/src/main/java/com/jaycekon/cloud/SpringBootCloudConsumerApplication.java
+++ spring-boot-cloud-consumer/src/main/java/com/jaycekon/cloud/SpringBootCloudConsumerApplication.java
@@ -6,7 +6,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
+/**
+ * Ribbon is a client side load balancer which gives you a lot of control over the behaviour of HTTP and TCP clients
+ * ribbon+restTemplate
+ * @author jettang
+ *
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class SpringBootCloudConsumerApplication {
--- spring-boot-cloud/src/main/java/com/jaycekon/cloud/SpringBootCloudApplication.java
+++ spring-boot-cloud/src/main/java/com/jaycekon/cloud/SpringBootCloudApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * EurekaServer
+ * @author jettang
+ *
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class SpringBootCloudApplication {
